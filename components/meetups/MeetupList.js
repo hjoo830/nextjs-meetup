d04a82636a8d291d3c6d0d37fdace02a78a8c5f4
hjoo830 <hjoo830@naver.com>
2024-12-02 01:42:53 +0900

    ✨ Feat: Add MeetupList component

---
+++ components/meetups/MeetupList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MeetupItem from "./MeetupItem";
+
+const MeetupList = ({ meetups }) => {
+  return (
+    <ul>
+      {meetups.map((meetup) => (
+        <MeetupItem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MeetupList;
