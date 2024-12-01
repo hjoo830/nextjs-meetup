691d0bde58fd973bccdbd4e832dac78030aed2a9
hjoo830 <hjoo830@naver.com>
2024-12-02 01:42:27 +0900

    ✨ Feat: Add server-side data fetching for meetups

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,36 @@
 import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ meetups }) {
   return (
     <>
       <Head>
         <title>모임 웹 사이트</title>
       </Head>
       <main>
-        <MeetupList />
+        <MeetupList meetups={meetups} />
       </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db("meetupDB");
+
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+  };
 }
