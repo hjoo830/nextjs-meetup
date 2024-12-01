c5930de374a196d7e55c0c4bc198cc0b8ba57c13
hjoo830 <hjoo830@naver.com>
2024-12-02 01:37:01 +0900

    ✨ Feat: Add NewMeetUpPage component

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,18 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React from "react";
+
+const NewMeetUpPage = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>모임 생성하기</title>
+      </Head>
+      <NewMeetupForm router={router} />
+    </>
+  );
+};
+
+export default NewMeetUpPage;
