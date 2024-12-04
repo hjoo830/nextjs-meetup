21badf4b76fe1e704d629c52c48663102f2a5c04
hjoo830 <hjoo830@naver.com>
2024-12-04 10:20:16 +0900

    💄 Design: Add styles for MeetupDetail

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,11 +1,15 @@
 import React from "react";
+import styles from "@/styles/MeetupDetail.module.css";
 
 const MeetupDetail = ({ image, title, address, description }) => {
   return (
-    <section>
-      <h1>{title}</h1>
-      <address>{address}</address>
-      <p>{description}</p>
+    <section className={styles.detail}>
+      <h1 className={styles.title}>{title}</h1>
+      <img src={image} alt={title} className={styles.img} />
+      <h3 className={styles.label}>모임 장소</h3>
+      <address className={styles.address}>{address}</address>
+      <h3 className={styles.label}>모임 소개</h3>
+      <p className={styles.description}>{description}</p>
     </section>
   );
 };
